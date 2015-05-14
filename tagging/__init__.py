cc8a82968431a29926995c48e947c1f30fc07cda
Fantomas42 <fantomas42@gmail.com>
2015-05-14 17:49:44 +0200

    Fix author type

--- tagging/__init__.py
+++ tagging/__init__.py
@@ -4,8 +4,8 @@ Django-tagging
 __version__ = '0.4.dev0'
 __license__ = 'BSD License'
 
-__author__ = 'Jonathan Buchanan',
-__author_email__ = 'jonathan.buchanan@gmail.com',
+__author__ = 'Jonathan Buchanan'
+__author_email__ = 'jonathan.buchanan@gmail.com'
 
 __maintainer__ = 'Fantomas42'
 __maintainer_email__ = 'fantomas42@gmail.com'
