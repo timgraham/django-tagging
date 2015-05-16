cfd6699e1bd5947031b75504a8d70d1b280c9564
Fantomas42 <fantomas42@gmail.com>
2015-05-16 22:10:00 +0200

    Create an AppConfig for tagging

--- tagging/__init__.py
+++ tagging/__init__.py
@@ -12,6 +12,7 @@ __maintainer_email__ = 'fantomas42@gmail.com'
 
 __url__ = 'https://github.com/Fantomas42/django-tagging'
 
+default_app_config = 'tagging.apps.TaggingConfig'
 
 class AlreadyRegistered(Exception):
     """
