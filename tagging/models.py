a6dcde080bf37bb7e3bd130c579be5bf70cdbc9a
Vinay Karanam <vinayinvicible@gmail.com>
2016-12-23 15:26:01 +0530

    Fixes IntegrityError while saving inconsistent tags

--- tagging/models.py
+++ tagging/models.py
@@ -50,7 +50,11 @@ class TagManager(models.Manager):
         for tag_name in updated_tag_names:
             if tag_name not in current_tag_names:
                 tag, created = self.get_or_create(name=tag_name)
-                TaggedItem._default_manager.create(tag=tag, object=obj)
+                TaggedItem._default_manager.get_or_create(
+                    content_type_id=ctype.pk,
+                    object_id=obj.pk,
+                    tag=tag,
+                )
 
     def add_tag(self, obj, tag_name):
         """
